e222f3fda690dc715723de990a644e0d09cd6f57
HoangTrung2004 <lenguyenhoangtrung2004@gmail.com>
2025-10-01 11:04:43 +0700

    edit Lab2

--- Lab2/src/main/java/web/controller/ProductController.java
+++ Lab2/src/main/java/web/controller/ProductController.java
@@ -1,36 +1,37 @@
 package web.controller;
 
 import java.util.*;                         // tạo nhanh danh sách mẫu
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.thymeleaf.model.IModel;
 import web.entity.Product;
 
 @Controller
 public class ProductController {
+    private List<Product> items = new ArrayList<>(Arrays.asList(
+            new Product("A", 1.0),
+            new Product("B", 12.0)
+    ));
+    private Product p1 = new Product("Iphone 30", 5000.0);
 
     @GetMapping("/product/form")
     public String form(Model model) {
-
-        Product p1 = new Product("iPhone 30", 5000.0);
         model.addAttribute("p1", p1);
         return "product/form";
     }
 
     @PostMapping("/product/save")
-    public String save(@ModelAttribute("p2") Product p2) {
+    public String save(@ModelAttribute("p2") Product p2, Model model) {
+        items.add(p2);
+        model.addAttribute("p1", p1);
+
         return "product/form";
     }
 
     @ModelAttribute("items")
     public List<Product> getItems() {
-        return Arrays.asList(
-                new Product("A", 1.0),
-                new Product("B", 12.0),
-                new Product("Iphone 17", 1000.0)
-        );
+        return items;
     }
 }
+
